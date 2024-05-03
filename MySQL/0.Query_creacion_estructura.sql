43b0dd6805bef6c68517e61b0c0ae04cbe0d4c35
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-05-03 11:48:10 +0200

    Update 0.Query_creacion_estructura.sql

--- MySQL/0.Query_creacion_estructura.sql
+++ MySQL/0.Query_creacion_estructura.sql
@@ -2,23 +2,25 @@
 
 CREATE TABLE `sverga`.`usuario` (
 `idusuario` INT NOT NULL AUTO_INCREMENT,
-`nombre` NVARCHAR(20) NOT NULL,
-`apellido1` NVARCHAR(20) NOT NULL,
-`apellido2` NVARCHAR(20) NOT NULL,
-`empleo` NVARCHAR(20) NOT NULL,
-`mail` NVARCHAR(20) NULL,
+`nombre` VARCHAR(20) NOT NULL,
+`apellido1` VARCHAR(20) NOT NULL,
+`apellido2` VARCHAR(20) NOT NULL,
+`empleo` VARCHAR(20) NOT NULL,
+`mail` VARCHAR(20) NULL,
 `telefono` INT NULL,
-PRIMARY KEY (`idusuario`));
+PRIMARY KEY (`idusuario`)
+);
 
 CREATE TABLE `sverga`.`dotacion` (
 `fk_usuario` INT NOT NULL,
-`departamento` NVARCHAR(20) NOT NULL,
+`departamento` VARCHAR(20) NOT NULL,
 PRIMARY KEY (`fk_usuario`),
-CONSTRAINT `fk_usuario`
-FOREIGN KEY (`fk_usuario_usuario`)
+CONSTRAINT `fk_usuario_dotacion`
+FOREIGN KEY (`fk_usuario`)
 REFERENCES `sverga`.`usuario` (`idusuario`)
 ON DELETE CASCADE
-ON UPDATE CASCADE);
+ON UPDATE CASCADE
+);
 
 CREATE TABLE `sverga`.`coordinador` (
 `fk_usuario` INT NOT NULL,
@@ -27,7 +29,8 @@ CONSTRAINT `fk_usuario_coordinador`
 FOREIGN KEY (`fk_usuario`)
 REFERENCES `sverga`.`usuario` (`idusuario`)
 ON DELETE CASCADE
-ON UPDATE CASCADE);
+ON UPDATE CASCADE
+);
 
 CREATE TABLE `sverga`.`administrador` (
 `fk_usuario` INT NOT NULL,
@@ -36,5 +39,5 @@ CONSTRAINT `fk_usuario_administrador`
 FOREIGN KEY (`fk_usuario`)
 REFERENCES `sverga`.`usuario` (`idusuario`)
 ON DELETE CASCADE
-ON UPDATE CASCADE);
-
+ON UPDATE CASCADE
+);
