4aca4927c02bf5551e3db3e6d808c3654fb2f165
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-05-09 11:53:39 +0200

    Update 0.Query_creacion_estructura.sql

--- MySQL/0.Query_creacion_estructura.sql
+++ MySQL/0.Query_creacion_estructura.sql
@@ -1,77 +1,86 @@
 // Creacion de tablas
 
-CREATE TABLE usuarios (
-	idusuario INT PRIMARY KEY,
-	nombre VARCHAR(20),
-	apellido1 VARCHAR(20),
-	apellido2 VARCHAR(20),
-	empleo VARCHAR(20),
-	mail VARCHAR(20),
-	telefono INT,
-	departamento VARCHAR(20),
-	prim_inicio BOOLEAN,
-	estado BOOLEAN,
-	contraseña VARCHAR(255)
+CREATE TABLE `aulas` (
+`idaula` varchar(20) NOT NULL,
+`edificio` varchar(20) DEFAULT NULL,
+`nombre` varchar(45) DEFAULT NULL,
+`capacidad` int DEFAULT NULL,
+`puestos` int DEFAULT NULL,
+`modulo` varchar(45) DEFAULT NULL,
+`zar` tinyint(1) DEFAULT NULL,
+`rap` tinyint(1) DEFAULT NULL,
+`red_ossorio` tinyint(1) DEFAULT NULL,
+`wan_pg` tinyint(1) DEFAULT NULL,
+`proyector` tinyint(1) DEFAULT NULL,
+`pizarra_digital` tinyint(1) DEFAULT NULL,
+`extras` varchar(255) DEFAULT NULL,
+PRIMARY KEY (`idaula`)
 );
 
-CREATE TABLE roles (
-	rol VARCHAR(20) PRIMARY KEY
+CREATE TABLE `calendario` (
+`tipo` varchar(20) NOT NULL,
+`num_hora` varchar(20) NOT NULL,
+`inicio` datetime DEFAULT NULL,
+`fin` datetime DEFAULT NULL,
+PRIMARY KEY (`tipo`,`num_hora`),
+UNIQUE KEY `tipo` (`tipo`),
+UNIQUE KEY `num_hora` (`num_hora`)
+) ;
+CREATE TABLE `usuarios` (
+`idusuario` int NOT NULL AUTO_INCREMENT,
+`nombre` varchar(20) NOT NULL,
+`apellido1` varchar(20) NOT NULL,
+`apellido2` varchar(20) NOT NULL,
+`empleo` varchar(20) DEFAULT NULL,
+`mail` varchar(45) DEFAULT NULL,
+`telefono` varchar(10) DEFAULT NULL,
+`departamento` varchar(20) DEFAULT NULL,
+`prim_inicio` tinyint(1) DEFAULT NULL,
+`estado` varchar(20) DEFAULT NULL,
+`clave` varchar(20) DEFAULT NULL,
+PRIMARY KEY (`idusuario`),
+UNIQUE KEY `mail_UNIQUE` (`mail`)
+) ;
+
+CREATE TABLE `reservas` (
+`idreserva` int NOT NULL AUTO_INCREMENT,
+`fk_solicitante` int NOT NULL,
+`fk_autorizador` int NOT NULL,
+`fk_aula` varchar(20) DEFAULT NULL,
+`num_alumnos` int DEFAULT NULL,
+`civil_militar` tinyint(1) DEFAULT NULL,
+`ejercito` varchar(20) DEFAULT NULL,
+PRIMARY KEY (`idreserva`),
+KEY `fk_solicitante` (`fk_solicitante`),
+KEY `fk_aula` (`fk_aula`),
+KEY `fk_autorizador` (`fk_autorizador`),
+CONSTRAINT `fk_aula` FOREIGN KEY (`fk_aula`) REFERENCES `aulas` (`idaula`) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT `fk_autorizador` FOREIGN KEY (`fk_autorizador`) REFERENCES `usuarios` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT `fk_solicitante` FOREIGN KEY (`fk_solicitante`) REFERENCES `usuarios` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE roles_usuarios (
-	idusuario INT,
-	rol VARCHAR(20),
-	FOREIGN KEY (idusuario) REFERENCES usuarios(idusuario),
-	FOREIGN KEY (rol) REFERENCES roles(rol),
-	PRIMARY KEY (idusuario, rol)
+CREATE TABLE `reservas_calendario` (
+`fk_reserva` int NOT NULL,
+`fk_tipo_cal` varchar(20) NOT NULL,
+`fk_num_hora_cal` varchar(20) NOT NULL,
+PRIMARY KEY (`fk_reserva`,`fk_tipo_cal`,`fk_num_hora_cal`),
+KEY `fk_tipo_cal_idx` (`fk_tipo_cal`),
+KEY `fk_num_hora_cal_idx` (`fk_num_hora_cal`),
+CONSTRAINT `fk_num_hora_cal` FOREIGN KEY (`fk_num_hora_cal`) REFERENCES `calendario` (`num_hora`) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT `fk_reserva` FOREIGN KEY (`fk_reserva`) REFERENCES `reservas` (`idreserva`) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT `fk_tipo_cal` FOREIGN KEY (`fk_tipo_cal`) REFERENCES `calendario` (`tipo`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE aulas (
-	idaula INT PRIMARY KEY,
-	edificio VARCHAR(20),
-	nombre VARCHAR(45),
-	capacidad INT,
-	puestos INT,
-	modulo VARCHAR(20),
-	zar BOOLEAN,
-	rap BOOLEAN,
-	red_ossorio BOOLEAN,
-	wan_pg BOOLEAN,
-	proyector BOOLEAN,
-	pizarra_digital BOOLEAN,
-	extras VARCHAR(255)
-);
+CREATE TABLE `roles` (
+`rol` varchar(20) NOT NULL,
+PRIMARY KEY (`rol`)
+) ;
 
-CREATE TABLE calendario (
-	tipo VARCHAR(20),
-	num_hora INT,
-	inicio DATETIME,
-	fin DATETIME,
-	PRIMARY KEY (tipo, num_hora),
-	INDEX (tipo),
-	INDEX (num_hora)
-);
-
-CREATE TABLE reservas (
-	idreserva INT PRIMARY KEY,
-	fk_solicitante INT,
-	fk_autorizador INT,
-	fk_aula INT,
-	num_alumnos INT,
-	civil_militar BOOLEAN,
-	ejercito VARCHAR(20),
-	FOREIGN KEY (fk_solicitante) REFERENCES usuarios(idusuario),
-	FOREIGN KEY (fk_autorizador) REFERENCES usuarios(idusuario),
-	FOREIGN KEY (fk_aula) REFERENCES aulas(idaula)
-);
-
-CREATE TABLE reservas_calendario (
-	fk_reserva INT,
-	fk_tipo_cal VARCHAR(20),
-	fk_num_hora_cal INT,
-	dia DATE,
-	FOREIGN KEY (fk_reserva) REFERENCES reservas(idreserva),
-	FOREIGN KEY (fk_tipo_cal) REFERENCES calendario(tipo),
-	FOREIGN KEY (fk_num_hora_cal) REFERENCES calendario(num_hora),
-	PRIMARY KEY (fk_reserva, fk_tipo_cal, fk_num_hora_cal, dia)
+CREATE TABLE `roles_usuarios` (
+`idusuario` int NOT NULL AUTO_INCREMENT,
+`rol` varchar(20) NOT NULL,
+KEY `idusuario` (`idusuario`),
+KEY `rol` (`rol`),
+CONSTRAINT `idusuario` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT `rol` FOREIGN KEY (`rol`) REFERENCES `roles` (`rol`) ON DELETE CASCADE ON UPDATE CASCADE
 );
